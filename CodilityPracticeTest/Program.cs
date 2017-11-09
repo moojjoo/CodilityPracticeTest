2e2cfb1bab0c04b992c95ae31f27583957218995
Robert Dannelly <robertdannelly@gmail.com>
2017-11-09 15:07:50 -0500

    Commit

--- CodilityPracticeTest/Program.cs
+++ CodilityPracticeTest/Program.cs
@@ -1,12 +1,46 @@
 ﻿using System;
+using System.Linq;
 
-namespace CodilityPracticeTest
-{
-    class Program
+class Solution
     {
-        static void Main(string[] args)
+        int[] A =  { 1,2,3,4,5,9};
+        public int solution(int[] A)
         {
-            Console.WriteLine("Hello World!");
+        A = new int[5];
+        A[0] = 1;
+        A[1] = 2;
+        A[2] = 3;
+        A[3] = 4;
+        A[4] = 5;
+
+        int value = 0;
+        int answer = 0;        
+        bool boolTest = true;
+        int lastNum = 0;
+
+        Array.Sort(A);
+
+        int i = 0;        
+        foreach (int num in A)
+        {
+                     
+
+            if ((num > 0) && (boolTest == true))
+            {          
+
+                value = num - lastNum;
+                if (value > 1)
+                {
+                    boolTest = false;
+                    answer = lastNum;
+                }
+            }
+            lastNum = num;
+            i++;
         }
+
+        return answer;
+        }   
+
     }
-}
+
---
+++ CodilityPracticeTest/Solution.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace CodilityPracticeTest
+{
+    class Solution
+    {
+        public int MySolution()
+        {
+            int[] A = { 1, 2, 3, 4, 5, 9 };
+            int value = 0;
+            int answer = 0;
+            bool boolTest = true;
+            int lastNum = 0;
+
+            Array.Sort(A);
+
+            int i = 0;
+            foreach (int num in A)
+            {
+
+
+                if ((num > 0) && (boolTest == true))
+                {
+
+                    value = num - lastNum;
+                    if (value > 1)
+                    {
+                        boolTest = false;
+                        answer = lastNum;
+                    }
+                }
+                lastNum = num;
+                i++;
+            }
+            return answer;
+        }
+    }
+}
+
+
+    
